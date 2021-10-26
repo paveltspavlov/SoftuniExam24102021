4742f97ce970f38bc6892e56f71458d5c64eb892
pavel <pavel_ts_pavlov@outlook.com>
2021-10-26 20:55:08 +0300

    final commit

--- HairSalon/Program.cs
+++ HairSalon/Program.cs
@@ -6,7 +6,50 @@ namespace HairSalon
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int target = int.Parse(Console.ReadLine());
+
+            double revenu = 0;
+            string haircut;
+
+            do
+            {
+                haircut = Console.ReadLine();
+
+                if (haircut == "haircut mens")
+                {
+                    revenu += 15;
+                }
+                else if (haircut == "haircut ladies")
+                {
+                    revenu += 20;
+                }
+                else if (haircut == "haircut kids")
+                {
+                    revenu += 10;
+                }
+                else if (haircut == "color touch up")
+                {
+                    revenu += 20;
+                }
+                else if (haircut == "color full color")
+                {
+                    revenu += 30;
+                }
+
+            } while (haircut != "closed" || target == revenu);
+
+            if (revenu == target)
+            {
+                Console.WriteLine("You have reached your target for the day!");
+                Console.WriteLine($"Earned money: {revenu}lv.");
+            }
+            else
+            {
+                double output = target - revenu;
+                Console.WriteLine($"Target not reached! You need {output}lv. more.");
+                Console.WriteLine($"Earned money: {revenu}lv.");
+            }
+
         }
     }
 }
